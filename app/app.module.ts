9daeac86007c02b42bca2e069cbc40fff8f07352
tqiqgq <tqiqgq@mail.ru>
2017-08-31 18:04:13 +0300

    create component

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,9 +1,11 @@
-import { NgModule } from '../node_modules/@angular/core';
-import { BrowserModule } from '../node_modules/@angular/platform-browser';
-
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
 
 @NgModule({
-	imports: [BrowserModule]
+	imports: [BrowserModule],
+	declarations: [AppComponent],
+	bootstrap: [AppComponent]
 })
 
 export class AppModule {
--- app/main.ts
+++ app/main.ts
@@ -1,4 +1,4 @@
-import {platformBrowserDynamic} from '../node_modules/@angular/platform-browser-dynamic';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app.module';
 
